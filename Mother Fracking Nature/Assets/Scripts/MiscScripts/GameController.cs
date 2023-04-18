15caa9500099b13403922375ea500da82c5ee1e1
ScuAt <116777229+ScuAt@users.noreply.github.com>
2023-04-17 19:31:09 -0500

    Basic Game Controller Script

---
+++ Mother Fracking Nature/Assets/Scripts/MiscScripts/GameController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public int enemyCount = 0;
+    public int enemyMax = 1;
+    public GameObject basicEnemy;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    IEnumerator SpawnEnemies()
+    {
+
+
+
+    }
+
+
+    IEnumerator RoundControl()
+    {
+
+
+
+    }
+}
